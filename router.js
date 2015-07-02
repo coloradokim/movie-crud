66d8bca343c70bacb498593f4ecc30ad01915582
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-07-02 09:49:16 -0600

    new page

--- router.js
+++ router.js
@@ -5,3 +5,37 @@ var routes = require('routes') (),
     qs = require('qs'),
     view = require('mustache'),
     mime = require('mime');
+
+routes.addRoute('/movie/new', function (req, res, url) {
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', 'text/html')
+    var file = fs.readFileSync('templates/movies/new.html')
+    var template = view.render(file.toString(), {})
+    res.end(template)
+  }
+  if (req.method === 'POST') {
+    var data = ''
+    req.on('data', function (chunk) {
+      data += chunk
+    })
+    req.on('end', function() {
+      var movie = qs.parse(data)
+      movie.insert(movie, function (err, doc) {
+        if (err) throw err
+        res.writeHead(302, {'Location': '/movies'})
+        res.end()
+      })
+    })
+  }
+})
+
+routes.addRoute('/movies', function (req, res, url) {
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', 'text/html')
+    movies.find({}, function (err, docs) {
+      var file = fs.readFileSync('templates/movies/index.html')
+      var template = view.render(file.toString(), {movies: docs})
+      res.end(template)
+    })
+  }
+})
